7bd073363a12256414004e87f48882eaefeefc7c
techicsdev <ja@sk-associates.org>
2020-02-27 19:16:30 +0600

    added transaction table

--- resources/views/vendor/backpack/base/dashboard.blade.php
+++ resources/views/vendor/backpack/base/dashboard.blade.php
@@ -20,8 +20,8 @@
 				<span class="info-box-icon bg-aqua"><i class="ion ion-ios-gear-outline"></i></span>
 
 				<div class="info-box-content">
-					<span class="info-box-text">CPU Traffic</span>
-					<span class="info-box-number">90<small>%</small></span>
+					<span class="info-box-text">Pending Orders</span>
+					<span class="info-box-number">{{ App\Models\Order::where('status_id',1)->sum('id') }}</span>
 				</div>
 				<!-- /.info-box-content -->
 			</div>
@@ -34,7 +34,7 @@
 
 				<div class="info-box-content">
 					<span class="info-box-text">Total Orders</span>
-					<span class="info-box-number">41,410</span>
+					<span class="info-box-number">{{ App\Models\Order::sum('id') }}</span>
 				</div>
 				<!-- /.info-box-content -->
 			</div>
@@ -50,7 +50,7 @@
 				<span class="info-box-icon bg-green"><i class="ion ion-ios-cart-outline"></i></span>
 
 				<div class="info-box-content">
-					<span class="info-box-text">Total Products</span>
+					<span class="info-box-text">Total Stock</span>
 					<span class="info-box-number">{{ App\Models\Product::sum('stock') }}</span>
 				</div>
 				<!-- /.info-box-content -->
